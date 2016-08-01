f8de53e26263d4ff090c2b544a5e7000ef4d4f94
Jonathan Kohn <dude16kh@gmail.com>
2016-08-01 08:45:35 -0400

    NewPost handler, Post class to create db entries

--- blog.py
+++ blog.py
@@ -5,7 +5,7 @@ import jinja2
 
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates_folder')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
@@ -34,17 +34,42 @@ class Handler (webapp2.RequestHandler):
         """
         self.write(self.render_str(template, **kw))
 
+class Post(db.Model):
+    subject = db.StringProperty(required=True) #particular type
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        print ("[!] render in Post")
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
 class NewPost(Handler):
     def get(self):
         #render form page
         self.render("newpost.html")
 
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            p = Post(subject=subject, content=content)
+            #add to the DB
+            p.put()
+
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            print ("[!] Error")
+            print (subject)
+
 
 
 app = webapp2.WSGIApplication([
                                 ("/blog/newpost", NewPost),
-                                ('/blog/([0-9]+)', PostPage),
-                                ('/blog/?', BlogFront),
+                                # ('/blog/([0-9]+)', PostPage),
+                                # ('/blog/?', BlogFront),
                             ], debug=True)
 
 
